5b69a839e1d8d10672dd5b76cce82907cbf4fb3d
Juan David <juandavid0306@hotmail.com>
2017-10-19 17:14:32 -0500

    adsasf

--- app/controllers/SuperController.java
+++ app/controllers/SuperController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.*;
 import play.libs.Json;
 import play.mvc.*;
@@ -131,6 +132,11 @@ public class SuperController extends Controller {
 
     @BodyParser.Of(BodyParser.Json.class)
     public Result rutaSecreta2(){
+
+        ObjectNode result = Json.newObject();
+        result.put("exampleField1", "foobar");
+        result.put("exampleField2", "Hello world!");
+
         JsonNode json = request().body().asJson();
         String nombre = json.findPath("nombre").textValue();
         if(nombre == null) {
