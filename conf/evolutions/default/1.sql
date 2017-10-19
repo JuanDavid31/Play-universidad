23726914658984aae3a46c607d1ac9fffe4a36d6
Juan David <juandavid0306@hotmail.com>
2017-10-19 11:26:58 -0500

    G

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,18 +3,18 @@
 
 # --- !Ups
 
+create table usuario (
+  c_id                          serial not null,
+  d_nombre                      varchar(255),
+  constraint pk_usuario primary key (c_id)
+);
+
 create table cancion (
   c_id                          serial not null,
   d_nombre                      varchar(255),
   d_uri                         varchar(255),
   c_id_usuario                  integer not null,
   constraint pk_cancion primary key (c_id)
-);
-
-create table usuario (
-  c_id                          serial not null,
-  d_nombre                      varchar(255),
-  constraint pk_usuario primary key (c_id)
 );
 
 alter table cancion add constraint fk_cancion_c_id_usuario foreign key (c_id_usuario) references usuario (c_id) on delete restrict on update restrict;
