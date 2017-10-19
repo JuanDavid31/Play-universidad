b5035fc0fb9bb18dec4ff5080d8454cd1f26be9f
Juan David <juandavid0306@hotmail.com>
2017-10-19 07:21:27 -0500

    bb

--- app/controllers/SuperController.java
+++ app/controllers/SuperController.java
@@ -1,7 +1,5 @@
 package controllers;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.*;
 import play.libs.Json;
 import play.mvc.*;
@@ -131,10 +129,7 @@ public class SuperController extends Controller {
     }
 
     public Result rutaSecreta2(){
-	    ObjectNode result = Json.newObject();
-	    result.put("exampleField1", "foobar");
-	    result.put("exampleField2", "Hello world!");
-	    return ok(result);
+	    return ok("fffgg");
     }
 
 }
